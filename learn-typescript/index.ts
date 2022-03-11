e6d410e2b77a1cb322f5e8b175decb763d065474
AndreLuizGP <andre.gomes@beedoo.com.br>
2022-03-11 20:49:24 -0300

    Aplicando interfaces

--- learn-typescript/index.ts
+++ learn-typescript/index.ts
@@ -1,1 +1,32 @@
-console.log('TypeScript');
+// types
+
+// intefaces auxilia no desenvolvimento para preparar ao longo do código
+interface Animal {
+  nome: string;
+  tipo: "Terrestre" | "Aquático";
+  executar_movimento( paraFrente: number): void;
+}
+
+interface Felino extends Animal {
+  cor: string;
+}
+
+const animal: Animal = {
+  nome: "Tigre",
+  tipo: "Terrestre",
+  executar_movimento: (paraFrente) => (`${animal.nome} andou ${paraFrente} metros`),
+    
+};
+
+
+
+const felino: Felino = {
+  nome: "Pantera",
+  tipo: "Terrestre",
+  cor: "Preto",
+  executar_movimento: (paraFrente) => (`${felino.nome} andou ${paraFrente} metros`),
+};
+
+function soma(a: number, b: number) {
+  return a + b;
+}
