032a03d85e9872808908890e67911f2af9e63abb
AndreLuizGP <andre.gomes@beedoo.com.br>
2022-03-11 20:59:21 -0300

    Aplicando Types

--- learn-typescript/index.ts
+++ learn-typescript/index.ts
@@ -1,32 +1,43 @@
-// types
-
-// intefaces auxilia no desenvolvimento para preparar ao longo do código
+// intefaces para classificar
 interface Animal {
   nome: string;
   tipo: "Terrestre" | "Aquático";
-  executar_movimento( paraFrente: number): void;
+  domestico: boolean;
+  executar_movimento(paraFrente: number): void;
 }
 
 interface Felino extends Animal {
   cor: string;
 }
 
-const animal: Animal = {
-  nome: "Tigre",
+interface Canino extends Animal {
+  porte: "pequeno" | "medio" | "grande";
+}
+
+// types para realizar junções
+type Domestico = Felino | Canino;
+
+const animal: Domestico = {
+  domestico: true,
+  nome: "Cachorro",
   tipo: "Terrestre",
-  executar_movimento: (paraFrente) => (`${animal.nome} andou ${paraFrente} metros`),
-    
+  porte: "medio",
+  executar_movimento: (paraFrente) =>
+    `${animal.nome} andou ${paraFrente} metros`,
 };
 
-
+/* const animal: Animal = {
+  nome: "Tigre",
+  tipo: "Terrestre",
+  executar_movimento: (paraFrente) =>
+    `${animal.nome} andou ${paraFrente} metros`,
+};
 
 const felino: Felino = {
   nome: "Pantera",
   tipo: "Terrestre",
   cor: "Preto",
-  executar_movimento: (paraFrente) => (`${felino.nome} andou ${paraFrente} metros`),
+  executar_movimento: (paraFrente) =>
+    `${felino.nome} andou ${paraFrente} metros`,
 };
-
-function soma(a: number, b: number) {
-  return a + b;
-}
+ */
