3d682e37f2e8a1dc60a32ceb7b0b1fd42d5aa1b1
AndreLuizGP <andre.gomes@beedoo.com.br>
2022-03-11 21:12:59 -0300

    Criando input controlado com typescript

--- learn-typescript/index.ts
+++ learn-typescript/index.ts
@@ -1,43 +1,50 @@
-// intefaces para classificar
-interface Animal {
-  nome: string;
-  tipo: "Terrestre" | "Aquático";
-  domestico: boolean;
-  executar_movimento(paraFrente: number): void;
-}
+const input = document.getElementById('input') as HTMLInputElement;
 
-interface Felino extends Animal {
-  cor: string;
-}
+input.addEventListener('input', (event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+})
 
-interface Canino extends Animal {
-  porte: "pequeno" | "medio" | "grande";
-}
 
-// types para realizar junções
-type Domestico = Felino | Canino;
+// // intefaces para classificar
+// interface Animal {
+//   nome: string;
+//   tipo: "Terrestre" | "Aquático";
+//   domestico: boolean;
+//   executar_movimento(paraFrente: number): void;
+// }
 
-const animal: Domestico = {
-  domestico: true,
-  nome: "Cachorro",
-  tipo: "Terrestre",
-  porte: "medio",
-  executar_movimento: (paraFrente) =>
-    `${animal.nome} andou ${paraFrente} metros`,
-};
+// interface Felino extends Animal {
+//   cor: string;
+// }
 
-/* const animal: Animal = {
-  nome: "Tigre",
-  tipo: "Terrestre",
-  executar_movimento: (paraFrente) =>
-    `${animal.nome} andou ${paraFrente} metros`,
-};
+// interface Canino extends Animal {
+//   porte: "pequeno" | "medio" | "grande";
+// }
 
-const felino: Felino = {
-  nome: "Pantera",
-  tipo: "Terrestre",
-  cor: "Preto",
-  executar_movimento: (paraFrente) =>
-    `${felino.nome} andou ${paraFrente} metros`,
-};
- */
+// // types para realizar junções
+// type Domestico = Felino | Canino;
+
+// const animal: Domestico = {
+//   domestico: true,
+//   nome: "Cachorro",
+//   tipo: "Terrestre",
+//   porte: "medio",
+//   executar_movimento: (paraFrente) =>
+//     `${animal.nome} andou ${paraFrente} metros`,
+// };
+
+// const animal: Animal = {
+//   nome: "Tigre",
+//   tipo: "Terrestre",
+//   executar_movimento: (paraFrente) =>
+//     `${animal.nome} andou ${paraFrente} metros`,
+// };
+
+// const felino: Felino = {
+//   nome: "Pantera",
+//   tipo: "Terrestre",
+//   cor: "Preto",
+//   executar_movimento: (paraFrente) =>
+//     `${felino.nome} andou ${paraFrente} metros`,
+// };
